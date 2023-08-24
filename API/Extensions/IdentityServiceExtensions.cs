7ee619c921a6dbe0f3d04492943dd7204cccd4ac
jingyi <niu.jy1203@gmail.com>
2023-08-24 14:04:45 +1200

    many to many migrations

--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -26,7 +26,9 @@ namespace API.Extensions
                     ValidateIssuerSigningKey = true,
                     IssuerSigningKey = key,
                     ValidateIssuer = false,
-                    ValidateAudience = false
+                    ValidateAudience = false,
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.Zero,
                 };
             });
             services.AddScoped<TokenService>();
--- Domain/Activity.cs
+++ Domain/Activity.cs
@@ -9,5 +9,6 @@ namespace Domain
         public string Category { get; set; }
         public string City { get; set; }
         public string Address { get; set; }
+        public ICollection<ActivityAttendee> Attendees { get; set; }
     }
 }
